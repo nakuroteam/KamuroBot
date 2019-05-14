a99c68162950d446f6d138156ec64fff6fc066f8
DorianMadi <dorianmadicontact@gmail.com>
2019-05-15 00:40:55 +0200

    First initializations

---
+++ index.js
@@ -0,0 +1,8 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+bot.on('ready', function () {
+    console.log("Je suis connecté !")
+  })
+
+bot.login('NTc3OTYzNjQyMTIxODc5NTgy.XNsx3A.aGWdmf746b8GF7SUEf05Dq850Y0');
