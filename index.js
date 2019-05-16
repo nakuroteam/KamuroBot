0f7539db8e3f702c8ad8b5f4fc2cea7b6d040bab
DorianMadi <dorianmadicontact@gmail.com>
2019-05-17 00:21:24 +0200

    Welcoming speech functionality

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
+/** IMPORTS **/
+//import { welcomeListener } from './welcome.js';
+const welcomeListener = require('./welcome.js').welcomeListener;
+
+/** Connexion to Discord **/
+
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
 bot.on('ready', function () {
-    console.log("Je suis connecté !")
-  })
+    console.log("Je suis connecté !");
+    
+});
 
 bot.login('NTc3OTYzNjQyMTIxODc5NTgy.XNsx3A.aGWdmf746b8GF7SUEf05Dq850Y0');
+
+/** Activation of every listeners **/
+
+welcomeListener(bot);
+
