2d370fd54b6a5b7d56515ea7eae3d24e1593a5fe
DorianMadi <dorianmadicontact@gmail.com>
2019-06-01 16:18:30 +0200

    Delete constants.js

--- constants.js
+++
@@ -1,9 +0,0 @@
-/**
- * List of npm install :
- * discord.js
- * ytdl-core
- * ffmpeg (+ installer avec apt-get install ffmpeg)
- * node-opus
- */
-
-exports.botID = "NTc3OTYzNjQyMTIxODc5NTgy.XPJ5FQ.vKpJxEYUgB5gfcqtKZ7KupUUfsA";
